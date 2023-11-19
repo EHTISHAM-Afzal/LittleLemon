fc3c15029c8339ff6566e8b93b8dbd6c0b14ab8c
EHTISHAM-Afzal <khaj3172@gmail.com>
2023-11-19 20:21:01 +0500

    modified:   LittleLemonBackend/App.js 	renamed:    LittleLemonBackend/Routes/catagories.js -> LittleLemonBackend/Routes/Categories.js

--- LittleLemonBackend/Routes/Categories.js
+++ LittleLemonBackend/Routes/Categories.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { Category, validatecategory } from "../Models/CatagoryModel.js";
+import { Category, validateCategory } from "../Models/CategoryModel.js";
 
 
 const catagoriesRouter = express.Router();
@@ -31,7 +31,7 @@ catagoriesRouter.post("/", (req, res) => {
     const category = new Category({
         name: req.body.name,
     });
-    const { error } = validatecategory(req.body);
+    const { error } = validateCategory(req.body);
     if (error) return res.status(400).send(error.message);
     category.save()
         .then((r) => res.send(r))
@@ -47,7 +47,7 @@ catagoriesRouter.post("/", (req, res) => {
 });
 
 catagoriesRouter.put("/:id", async (req, res) => {
-    const { error } = validatecategory(req.body);
+    const { error } = validateCategory(req.body);
     if (error) return res.status(400).send(error.message);
     await Category
         .findByIdAndUpdate(req.params.id, req.body, { new: true })
