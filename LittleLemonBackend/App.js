98e15ff86ef51af9d73e3b50c8fb12ac40e5579b
EHTISHAM-Afzal <khaj3172@gmail.com>
2023-11-19 08:41:36 +0500

    changes on ProductModel and many more 	modified:   LittleLemonBackend/App.js 	new file:   LittleLemonBackend/Models/CartModel.js 	renamed:    LittleLemonBackend/Models/DishModel.js -> LittleLemonBackend/Models/ProductModel.js 	new file:   LittleLemonBackend/Models/UserModel.js 	new file:   LittleLemonBackend/Routes/Cart.js 	deleted:    LittleLemonBackend/Routes/Dish.js 	new file:   LittleLemonBackend/Routes/Product.js 	new file:   LittleLemonBackend/Routes/User.js 	modified:   LittleLemonBackend/Routes/catagories.js 	modified:   LittleLemonFrontEnd

--- LittleLemonBackend/App.js
+++ LittleLemonBackend/App.js
@@ -1,11 +1,12 @@
 import express from "express";
 import morgan from 'morgan';
-import DishRoutes from './Routes/Dish.js'
+import ProductRoutes from './Routes/Product.js'
 import catagoriesRouter from './Routes/catagories.js';
 import connectDB from './MongoDB/Connect.js';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 import TablesRoutes from "./Routes/BookingTable.js";
+import UserRoutes from "./Routes/User.js";
 
 
 dotenv.config();
@@ -14,9 +15,10 @@ const app = express();
 app.use(express.json({ limit: "25mb" }));
 app.use(cors());
 app.use(morgan("dev"));
-app.use("/api/dishes", DishRoutes);
+app.use("/api/products", ProductRoutes);
 app.use("/api/catagories", catagoriesRouter);
 app.use("/api/tables", TablesRoutes );
+app.use("/api/users", UserRoutes );
 
 
 const port = process.env.PORT || 3002;
